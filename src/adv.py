7904be03006891156d2b5c5df30ef79e797a107a
unknown <prakashbahadursingh2@gmail.com>
2020-08-06 09:11:14 -0700

    player and room class addded and some functionality to start hte game

--- src/adv.py
+++ src/adv.py
@@ -1,51 +1,98 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
-
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    "outside":  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    "foyer":    Room("Foyer", """Dim light filters in from the south. Dusty
+                     passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    "overlook": Room("Grand Overlook", """A steep cliff appears before you, falling
+                    into the darkness. Ahead to the north, a light flickers in
+                    the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    "narrow":   Room("Narrow Passage", """The narrow passage bends here from west
+                    to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    "treasure": Room("Treasure Chamber", """You"ve found the long-lost treasure
+                    chamber! Sadly, it has already been completely emptied by
+                    earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room["outside"].n_to = room["foyer"]
+room["foyer"].s_to = room["outside"]
+room["foyer"].n_to = room["overlook"]
+room["foyer"].e_to = room["narrow"]
+room["overlook"].s_to = room["foyer"]
+room["narrow"].w_to = room["foyer"]
+room["narrow"].n_to = room["treasure"]
+room["treasure"].s_to = room["narrow"]
 
 #
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+# Make a new player object that is currently in the "outside" room.
+
+# getting player information
+name = input("what is your name player???").lower().strip()
+player = Player(name, room["outside"])
+print("Welcome to the adventure game")
+# name = input("what is your name player???").lower().strip()
+# player.name = name
+
+# starts
+print(f"Hello {name}! Let's explore the rooms")
+answer = input("Shall we begin? y/n: ")
+if answer.lower().strip() == "y":
+    print("lets start the game!!!")
+    print(f"You are currently in {player.current_room} room")
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+while True:
+    #
+    # * Prints the current room name
+    new_player = Player(name, room["outside"])
+    print(new_player)
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+    print(new_player.current_room.description)
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn"t allowed.
+    inputs = ["n", "s", "e", "w", "q"]
+    # If the user enters "q", quit the game.
+    command = input("> ").split(",")
+
+    if command[0] == "q":
+        answer = input("are you sure: y/n: ")
+        if(answer == "y"):
+            sys.exit()
+        continue
+    elif command[0] == "n":
+        if(new_player.current_room.n_to != None):
+            print("Moving north to Foyer room.")
+            new_player.current_room = player.current_room.n_to
+        else:
+            print("Can not go this way, try another way!!!")
+    elif command[0] == "s":
+        if(new_player.current_room.s_to != None):
+            new_player.current_room = player.current_room.s_to
+        else:
+            print("Can not go this way, try another way!!!")        
+    elif command[0] == "e":
+        if(new_player.current_room.e_to != None):
+            new_player.current_room = player.current_room.e_to
+        else:
+            print("Can not go this way, try another way!!!") 
+    elif command[0] == "w":
+        if(new_player.current_room.w_to != None):
+            new_player.current_room = player.current_room.w_to
+        else:
+            print("Can not go this way, try another way!!!")
+
+# elif command[0] == "n":
+# # check if the player can move to the north 
+# # if there is, set that north room as the player"s location
